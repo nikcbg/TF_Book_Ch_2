2c6b794b75d195683a8192ea7c634ef2da03544d
nikolay <nikcbg@yahoo.com>
2019-06-29 13:43:33 +0300

    update tf code to 0.12

--- one_webserver/main.tf
+++ one_webserver/main.tf
@@ -5,15 +5,16 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami                    = "ami-40d28157"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
-              EOF
+EOF
 
-  tags {
+
+  tags = {
     Name = "terraform-example"
   }
 }
@@ -22,13 +23,14 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
+  value = aws_instance.example.public_ip
 }
+
